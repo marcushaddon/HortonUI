5e8a6afee8ccd9eff1125627cde56e1b16f60dd3
marcushaddon <haddon.marcus@gmail.com>
2017-04-01 19:22:05 -0500

    Style things

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // get some things straight
 
-$('#input').html("I\'m a simple little predictive text engine. You can type or paste text into this text box, and I can read it and learn words and associations. Since I'm written in Javascript, and I run inside the browser, I forget everything I've learned everytime the window is refreshed. This means whatever text you feed me will have big impact on the types of words I predict/poems I write/etc. You can click on me to have me read this text, to start!!!");
+$('#input').html("I\'m a simple little predictive text engine. You can type or paste text into this text box, and I can read it and learn words and associations. Since I run inside the browser, I forget everything I've learned everytime the window is refreshed. This means whatever text you feed me will have big impact on the types of words I predict/poems I write/etc. You can click on me to have me read this text, to start!!!");
 
 // give horton instructions on how to cooperate
 var wordsLearned = 0;
@@ -23,7 +23,7 @@ horton.onStrengthenAssoc = function() {
 }
 
 // hook up our UI
-$('#horton').on('click', function(){
+$('#readbutton').on('click', function(){
 	var input = $('#input').val();
 	$('#format').val(input); // hacky way of getting rid of all these line breaks
 	input = $('#format').val();
